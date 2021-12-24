22b05141b9dc556abdc27b7fb84c5565479f0aa0
mjmorrison10 <86391005+mjmorrison10@users.noreply.github.com>
2021-12-23 16:23:39 -0800

    fetch API
    Added fetch functionality using async/await and created a copy of the data

--- dist/index.4bfdf5c7.js
+++
@@ -1,2 +0,0 @@
-const recipeContainer=document.querySelector(".recipe"),timeout=function(e){return new Promise((function(o,t){setTimeout((function(){t(new Error(`Request took too long! Timeout after ${e} second`))}),1e3*e)}))};console.log("test");
-//# sourceMappingURL=index.4bfdf5c7.js.map
--- src/js/controller.js
+++ src/js/controller.js
@@ -12,4 +12,38 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
-console.log('test');
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc5b'
+      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+
+    if(!res.ok) throw new Error(`${data.message} (${res.status})`)
+
+    console.log(res);
+
+    console.log(data);
+
+    let {recipe} = data.data
+
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients
+    }
+
+    console.log(recipe);
+
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+showRecipe();
