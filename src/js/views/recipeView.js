e049133eab6a2a3488d0f29fd9ad47566fa5d2a7
mjmorrison10 <86391005+mjmorrison10@users.noreply.github.com>
2021-12-26 14:53:12 -0800

    remove fractional

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,5 @@
 import View from './View.js';
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -11,7 +10,7 @@ class RecipeView extends View {
     ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
   }
 
-  _generateMarkup() { 
+  _generateMarkup() {
     return `
         <figure class="recipe__fig">
           <img src="${this._data.image}" alt="${
@@ -102,9 +101,7 @@ class RecipeView extends View {
   <svg class="recipe__icon">
     <use href="${icons}#icon-check"></use>
   </svg>
-  <div class="recipe__quantity">${
-    ing.quantity ? new Fraction(ing.quantity).toString() : ''
-  }</div>
+  <div class="recipe__quantity">${ing.quantity}</div>
     <div class="recipe__description">
       <span class="recipe__unit">${ing.unit}</span>
       ${ing.description}
@@ -115,3 +112,5 @@ class RecipeView extends View {
 }
 
 export default new RecipeView();
+
+// ing.quantity ? new Fraction(ing.quantity).toString() : ''
